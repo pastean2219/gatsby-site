d2e741ee1d3e8f005e29932b26da9d060f57c562
Pastean Patriciu <pastean.patriciu@gmail.com>
2022-10-20 13:34:15 +0300

    added one Line

--- test/src/pages/index.js
+++ test/src/pages/index.js
@@ -2,5 +2,10 @@ import React from "react"
 import "./index.css"
 
 export default function Home() {
-  return <div className="hello">Hello world!</div>
+  return (
+    <div className="hello">
+      <div>Hello world!</div>
+      <div>Hello world!</div>
+    </div>
+  )
 }
